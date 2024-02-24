e42b590c2945eea1ef044dd7e244afce0b0c029d
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-25 02:53:24 +0530

    Add CORS and JSON parsing middleware, and create a basic route handler

--- server/index.js
+++ server/index.js
@@ -4,4 +4,20 @@ import cors from 'cors'
 
 dotenv.config()
 
-const app = express ()
+const app = express()
+app.use(cors())
+app.use(express.json({
+    limit: '50mb'
+}))
+
+app.get('/', async (req, res) => {
+    res.send('Hello World')
+})
+
+const startServer = async (req, res) => {
+    app.listen(8080, () => {
+        console.log("server started on 8080")
+    })
+}
+
+startServer();
