b784a83069a153dd64a1d6f6555efd0163bbeac7
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-25 03:55:39 +0530

    Add post and artistic routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+
 import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import artisticRoutes from './routes/artisticRoutes.js'
 
 dotenv.config()
 
@@ -11,13 +14,16 @@ app.use(express.json({
     limit: '50mb'
 }))
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/artistic', artisticRoutes);
+
 app.get('/', async (req, res) => {
     res.send('Hello World')
 })
 
 const startServer = async (req, res) => {
     try {
-        connectDB(process.env.MONGODBURL)
+        connectDB(process.env.MONGODB_URL)
         app.listen(8080, () => {
             console.log("server started on 8080")
         })
